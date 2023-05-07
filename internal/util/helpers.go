eea49950f0f8352d42f7bf2e836c7e3c769aa0e2
Jim Schubert <james.schubert@gmail.com>
2023-05-07 10:42:33 -0400

    Add path cleanup helper

---
+++ internal/util/helpers.go
@@ -0,0 +1,22 @@
+package util
+
+import "regexp"
+
+var nonPathRegex = regexp.MustCompile(`[^a-zA-Z0-9._\- ]+`)
+
+// CleanPath will replace all invalid characters in input with underscore by default. A varargs is abused here to allow
+// for passing a single optional replace character (acting as a function overload).
+func CleanPath(input string, replace ...string) string {
+	var replaceWith string
+	if len(replace) > 1 {
+		panic("multiple replace characters are not supported")
+	}
+
+	if len(replace) == 1 {
+		replaceWith = replace[0]
+	} else {
+		replaceWith = "_"
+	}
+	
+	return nonPathRegex.ReplaceAllString(input, replaceWith)
+}
---
+++ internal/util/helpers_test.go
@@ -0,0 +1,43 @@
+package util
+
+import "testing"
+
+func TestCleanPath(t *testing.T) {
+	type args struct {
+		input   string
+		replace []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "no replacement for valid string",
+			args: args{input: "This.is.valid"},
+			want: "This.is.valid",
+		},
+		{
+			name: "replacement for unexpected characters",
+			args: args{input: "first/second/third/last"},
+			want: "first_second_third_last",
+		},
+		{
+			name: "no replacement for valid string when passed custom replace override",
+			args: args{input: "This.is.valid", replace: []string{"|"}},
+			want: "This.is.valid",
+		},
+		{
+			name: "replacement for unexpected characters when passed custom replace override",
+			args: args{input: "first/second/third/last", replace: []string{"-"}},
+			want: "first-second-third-last",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CleanPath(tt.args.input, tt.args.replace...); got != tt.want {
+				t.Errorf("CleanPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
